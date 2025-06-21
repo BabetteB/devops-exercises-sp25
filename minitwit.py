22638b21ad1eb2aad11c7b3bb2f26a20a72c45f9
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2025-06-21 15:17:57 +0200

    week01: chore: update application from 2to3

--- minitwit.py
+++ minitwit.py
@@ -9,6 +9,7 @@
     :license: BSD, see LICENSE for more details.
 """
 
+import os
 import re
 import time
 import sqlite3
@@ -21,7 +22,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 
 # configuration
-DATABASE = '/tmp/minitwit.db'
+DATABASE = os.path.join(os.path.dirname(__file__), 'minitwit.db')
 PER_PAGE = 30
 DEBUG = True
 SECRET_KEY = 'development key'
@@ -39,7 +40,7 @@ def init_db():
     """Creates the database tables."""
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
+                db.cursor().executescript(f.read().decode('utf-8'))
         db.commit()
 
 
@@ -94,7 +95,7 @@ def timeline():
     redirect to the public timeline.  This timeline shows the user's
     messages as well as all the messages of followed users.
     """
-    print("We got a visitor from: " + str(request.remote_addr))
+    print(("We got a visitor from: " + str(request.remote_addr)))
     if not g.user:
         return redirect(url_for('public_timeline'))
     offset = request.args.get('offset', type=int)
